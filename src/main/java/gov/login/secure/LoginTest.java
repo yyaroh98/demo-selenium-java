dd7800c8235c4619aef2ff7ed503a5ee2388f2e2
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-24 13:17:14 +0300

    add test to validate login form with any email

--- src/main/java/gov/login/secure/LoginTest.java
+++ src/main/java/gov/login/secure/LoginTest.java
@@ -7,12 +7,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
+
     @Test
     public void test1() {
         WebDriver webDriver = new ChromeDriver();
         webDriver.get("https://secure.login.gov/");
 
         String buttonSingInXpath = "//*[@id=\"main-content\"]/div/nav/ul/li[1]/a";
+        By buttonSingInBy = By.xpath(buttonSingInXpath);
+        WebElement buttonSingInWebElement = webDriver.findElement(buttonSingInBy);
+        buttonSingInWebElement.click();
+    }
+
+    @Test
+    public void test2() {
+        WebDriver webDriver = new ChromeDriver();
+        webDriver.get("https://secure.login.gov/");
+
+        String inputEmailXpath = "//*[@id=\"user_email\"]";
+        By inputEmailBy = By.xpath(inputEmailXpath);
+        WebElement inputEmailWebElement = webDriver.findElement(inputEmailBy);
+        inputEmailWebElement.sendKeys("testA@test.com");
+
+        String buttonSingInXpath = "//*[@id=\"new_user\"]/lg-submit-button/button";
         By buttonSingInBy = By.xpath(buttonSingInXpath);
         WebElement buttonSingInWebElement = webDriver.findElement(buttonSingInBy);
         buttonSingInWebElement.click();
