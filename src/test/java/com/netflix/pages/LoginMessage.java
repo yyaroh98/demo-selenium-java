57c1d3ab6b9d6c837686eec7a7ed57ba50cb8251
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-02 14:35:11 +0300

    add login message

--- src/test/java/com/netflix/pages/LoginMessage.java
+++ src/test/java/com/netflix/pages/LoginMessage.java
@@ -1,4 +1,7 @@
 package com.netflix.pages;
 
 public class LoginMessage {
+    public static final String EMPTY_FIELD_LOGIN = "Укажите действительный адрес электронной почты или номер телефона.";
+    public static final String EMPTY_FIELD_PASSWORD = "Пароль должен содержать от 4 до 60 символов.";
+    public static final String WRONG_CREDENTIALS = "Неверный пароль для ";
 }
