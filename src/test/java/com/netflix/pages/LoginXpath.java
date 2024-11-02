aa49c57e70361f6c02d97f219ee3337c57fe928f
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-02 14:34:53 +0300

    add Xpath

--- src/test/java/com/netflix/pages/LoginXpath.java
+++ src/test/java/com/netflix/pages/LoginXpath.java
@@ -1,4 +1,12 @@
 package com.netflix.pages;
 
+import org.openqa.selenium.By;
+
 public class LoginXpath {
+    public static final String INPUT_LOGIN = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div/input";
+    public static final String INPUT_PASSWORD = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[1]/input";
+    public static final String SIGN_IN_BUTTON = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
+    public static final String EMAIL_FIELD_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
+    public static final String PASSWORD_FIELD_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
+    public static final String WRONG_CREDENTIALS_ERROR_MESSAGE = "/html/body/div[1]/div/div/div[2]/div/header/div/div/div/div";
 }
