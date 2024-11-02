92fa37abfc2e6df9ce0c386332b9af3a075ce6ef
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-11-02 14:56:46 +0300

    add messages

---
+++ src/test/java/by/emall/pages/LoginMessage.java
@@ -0,0 +1,10 @@
+package by.emall.pages;
+
+public class LoginMessage {
+    public static final String EMPTY_FIELD_LOGIN_AND_PASSWORD = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)" +
+            "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String EMPTY_FIELD_LOGIN = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
+    public static final String EMPTY_FIELD_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String WRONG_CREDENTIALS = "Неверный логин или пароль";
+
+}
